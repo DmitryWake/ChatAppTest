9ba39ae97de2bf76b376ba38f84c2c2e3ee8cb0f
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-08-02 18:08:26 +0300

    Статус в сети и гит

--- app/src/main/java/com/example/chatapp/utilities/AppStates.kt
+++ app/src/main/java/com/example/chatapp/utilities/AppStates.kt
@@ -5,16 +5,13 @@ enum class AppStates(val state: String) {
     OFFLINE("Был недавно"),
     TYPING("Печатает...");
 
-    companion object {
-        fun updateState(appStates: AppStates) {
+    companion object{
+        fun updateState(appStates: AppStates){
+            /*Функция принимает состояние и записывает в базу данных*/
             REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID).child(CHILD_STATE)
                 .setValue(appStates.state)
-                .addOnSuccessListener {
-                    USER.state = appStates.state
-                }
-                .addOnFailureListener {
-                    showToast(it.message.toString())
-                }
+                .addOnSuccessListener { USER.state = appStates.state }
+                .addOnFailureListener { showToast(it.message.toString()) }
         }
     }
 }
