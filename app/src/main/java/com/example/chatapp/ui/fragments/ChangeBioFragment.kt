149243f8e85d23f31698636825b44c6bf9063c57
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-09-03 14:30:08 +0300

    Sum up

---
+++ app/src/main/java/com/example/chatapp/ui/fragments/ChangeBioFragment.kt
@@ -0,0 +1,21 @@
+package com.example.chatapp.ui.fragments
+
+import com.example.chatapp.R
+import com.example.chatapp.database.*
+import com.example.chatapp.utilities.*
+import kotlinx.android.synthetic.main.fragment_change_bio.*
+
+
+class ChangeBioFragment : BaseChangeFragment(R.layout.fragment_change_bio) {
+
+    override fun onResume() {
+        super.onResume()
+        settings_input_bio.setText(USER.bio)
+    }
+
+    override fun change() {
+        super.change()
+        val newBio = settings_input_bio.text.toString()
+        setBioToDatabase(newBio)
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/ui/fragments/ChatFragment.kt
@@ -0,0 +1,14 @@
+package com.example.chatapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.chatapp.R
+
+
+class ChatFragment :  Fragment(R.layout.fragment_chat) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+
+}
